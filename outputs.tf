bd86c36a81941a7ee9d125c93cc8805f03d73ff2
timotapanainen@users.noreply.github.com <timo.tapanainen@gmail.com>
2018-02-19 12:09:52 +0200

    family and revision outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "arn" {
   value = "${aws_ecs_task_definition.task_definition.arn}"
 }
 
+output "family" {
+  value = "${aws_ecs_task_definition.task_definition.family}"
+}
+
+output "revision" {
+  value = "${aws_ecs_task_definition.task_definition.revision}"
+}
+
 output "role_id" {
   value = "${aws_iam_role.task_role.id}"
 }
